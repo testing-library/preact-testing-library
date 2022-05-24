6281caaee0e4ad8937dc377f0ba544b9b0721eed
Mike Robinson <mikerob215@protonmail.com>
2022-05-24 16:51:20 -0400

    Update src/file-event.js
    Co-authored-by: Jovi De Croock <decroockjovi@gmail.com>

--- src/file-event.js
+++ src/file-event.js
@@ -6,13 +6,9 @@ export const fireEvent = (...args) => domFireEvent(...args)
 
 Object.keys(domFireEvent).forEach((key) => {
   fireEvent[key] = (elem) => {
-    // when preact sets up an event listener it uses this check
-    // `'on' + eventName.toLowerCase() in dom`,
-    // to determine eventName from prop and this works in browser, but in jsdom if an event
-    // isn't supported, preact's check will
-    // fail and lead it to dispatch an event like `PointerStart` instead of `pointerStart`
-
-    // Here we can duplicate the check in preact and case the way preact will
+    // Preact registers event-listeners in lower-case, so onPointerStart becomes pointerStart
+    // here we will copy this behavior, when we fire an element we will fire it in lowercase so we hit
+    // the Preact listeners.
     const eventName = `on${key.toLowerCase()}`
     const isInElem = eventName in elem
     return isInElem
